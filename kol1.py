e5829329673f69f5fc9392f392593dacf2d0088e
Christos Spyropoulos <xristosandroid@gmail.com>
2017-04-05 01:32:42 +0200

    Full solution.2 files(ko11.py and matrix_library.py

--- kol1.py
+++ kol1.py
@@ -1,23 +1,10 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be a fork from https://github.com/mwmajew/2017sum_era1_kol1
-#
-#Delete these comments before commit!
-#Good luck.
+from matrix_library import Matrix_representator
+
+print "--------Task1:--------" 
+matrix_object1 = Matrix_representator([[1, 2], [3, 4]])
+print "" 
+matrix_object2 = Matrix_representator([[0.5, 10], [100, 1000]])
+print "\n--------Task2:--------" 
+matrix_object_of_sum = Matrix_representator(matrix_object1.add(matrix_object2))
+print "\n--------Task3:--------"
+matrix_object_of_multiply = Matrix_representator(matrix_object1.multiply_with(matrix_object2))
